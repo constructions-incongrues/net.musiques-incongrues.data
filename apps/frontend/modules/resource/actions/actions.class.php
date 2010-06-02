7e0fa0dd0017f6a811b017ddcd16886a11cce0ac
Tristan Rivoallan <tristan@rivoallan.net>
2010-06-02 17:44:36 +0200

    [frontend] Unique results hack now clearly marked as hack.

--- apps/frontend/modules/resource/actions/actions.class.php
+++ apps/frontend/modules/resource/actions/actions.class.php
@@ -60,15 +60,19 @@ class resourceActions extends sfActions
     $resource_group_instance = new $resource_group_class($this->getContext()->getEventDispatcher(), sfLucene::getInstance('IndexA', 'fr'));
     $raw_results = $resource_group_instance->search($request->getParameterHolder());
     
-    // Make sure results are unique (TODO : this should go in search class)
+    // Make sure results are unique (this a just a hack)
     // see http://www.php.net/manual/en/function.array-unique.php#91134
-    // TODO : it sucks as it makes the "limit" parameter not trustable
-    $raw_results_unique = array();
-    foreach ($raw_results as $result)
+    // NOTE : it sucks as it makes the "limit" parameter not trustable
+    // TODO : resource unicity must be enforced when importing documents into solr
+    if ($this->getRequestParameter('hack_unique_results', false))
     {
-      $raw_results_unique[md5($result['url'])] = $result;
+      $raw_results_unique = array();
+      foreach ($raw_results as $result)
+      {
+        $raw_results_unique[md5($result['url'])] = $result;
+      }
+      $raw_results = $raw_results_unique;
     }
-    $raw_results = $raw_results_unique;
     
     // Format results
     $formatter_class = sprintf('CI_Search_Formatter_%s', ucfirst($format));
