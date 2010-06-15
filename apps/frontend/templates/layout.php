0fa067bc9f89963decb4d7a8ffa28a815a2dd783
Tristan Rivoallan <tristan@rivoallan.net>
2010-06-15 15:21:01 +0200

    [frontend] Added Google Analytics tracking code.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -25,5 +25,19 @@
     <p>Ce service utilise (notamment) <a href="http://www.symfony-project.org">symfony</a>, <a href="http://www.doctrine-project.org">Doctrine</a> et <a href="http://lucene.apache.org/solr/">Solr</a>.</p>
     <p>Contact : contact @ musiques-incongrues . net</p>
 
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-16967840-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+
   </body>
 </html>
