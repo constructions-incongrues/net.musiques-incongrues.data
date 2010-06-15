71a774cdd42ced68200076f9218cd39f4156c89b
Tristan Rivoallan <tristan@rivoallan.net>
2010-06-15 15:22:18 +0200

    [cli]  * Enforced link uniqueness in miner:extract-links task and model  * Added miner:extract-links options : progress, verbose  * Made sure ftp URLs are also extracted

--- lib/task/minerExtractlinksTask.class.php
+++ lib/task/minerExtractlinksTask.class.php
@@ -11,6 +11,8 @@ class minerExtractlinksTask extends sfBaseTask
     $this->addOptions(array(
     new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
     new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+    new sfCommandOption('progress', null, sfCommandOption::PARAMETER_NONE, 'Display a progress bar'),
+    new sfCommandOption('verbose', null, sfCommandOption::PARAMETER_NONE, 'Display more informations about extraction process'),
     ));
 
     $this->namespace        = 'miner';
@@ -34,11 +36,11 @@ EOF;
     $connection = $database->getDoctrineConnection();
 
     // Fetch all links from datasource
-    // TODO : do not extract deleted comments nor whispers
     $q = 'select c.CommentID, c.Body, c.DateCreated, c.AuthUserID, c.DiscussionID, d.Name as DiscussionName, u.Name
-    	from LUM_Comment c 
+    	from LUM_Comment c
     	inner join LUM_User u on c.AuthUserID = u.UserID
-    	inner join LUM_Discussion d on c.DiscussionID = d.DiscussionID';
+    	inner join LUM_Discussion d on c.DiscussionID = d.DiscussionID
+        where c.Deleted = "0" and c.WhisperUserID = 0';
     $comments = $connection->fetchAssoc($q);
 
     // Log
@@ -50,30 +52,40 @@ EOF;
 
     // For logging purposes
     $urls_found_count = 0;
+    $urls_inserted_count = 0;
 
     // Extract links from each comment
     $comments_parsed = 0;
-    // TODO : make this optional
-    include 'Console/ProgressBar.php';
-    $progress_bar = new Console_ProgressBar(
+
+    if ($options['progress'])
+    {
+      include 'Console/ProgressBar.php';
+      $progress_bar = new Console_ProgressBar(
       '** '.$arguments['dsn'].' %fraction% comments [%bar%] %percent%',
       '=>', '-', 80, count($comments), array('ansi_terminal' => true)
-    );
+      );
+    }
 
     foreach ($comments as $comment)
     {
-      // Update progress bar
-      $progress_bar->update(++$comments_parsed);
-      
-      $matches = array();
-      preg_match_all('#\bhttps?://[-A-Z0-9+&@\#/%?=~_|!:,.;]*[-A-Z0-9+&@\#/%=~_|]#i', $comment['Body'], $matches);
-      $urls_found = $matches[0];
+      if ($options['progress'])
+      {
+        // Update progress bar
+        $progress_bar->update(++$comments_parsed);
+      }
 
+      // Match URLs in comment body
+      $matches = array();
+      preg_match_all('#\b..?tps?://[-A-Z0-9+&@\#/%?=~_|!:,.;]*[-A-Z0-9+&@\#/%=~_|]#i', $comment['Body'], $matches);
+      $urls_found = $matches[0];
 
       // If links are found, insert them into database
       if (count($urls_found))
       {
-        //$this->logSection('info', sprintf('Found %d urls in comment %d. Let\'s add them to database.'  , count($urls_found), $comment['CommentID']));
+        if ($options['verbose'])
+        {
+          $this->logSection('info', sprintf('Found %d urls in comment %d. Let\'s add them to database.'  , count($urls_found), $comment['CommentID']));
+        }
         $urls_found_count += count($urls_found);
         foreach ($urls_found as $url)
         {
@@ -90,16 +102,34 @@ EOF;
           {
             $link->autoPopulate($extensions_mimetype_map);
             $link->save();
+            $urls_inserted_count++;
           }
+          // Invalid URLs, thrown by $link->autoPopulate()
           catch (InvalidArgumentException $e)
           {
             $this->logSection('error', $e->getMessage());
+          }
+          // Duplicate URLs
+          catch (Doctrine_Connection_Mysql_Exception $e)
+          {
+            if ($e->getPortableCode() === Doctrine_Core::ERR_ALREADY_EXISTS)
+            {
+              if ($options['verbose'])
+              {
+                // TODO : skipping insertion is not ideal as we lose meta informations : link existing in several discussion, posted by several users, etc.
+                $this->logSection('info', sprintf('Found duplicate link "%s" in discussion "%d". Skipping insertion', $link->url, $link->discussion_id));
+              }
+            }
+            else
+            {
+              throw $e;
+            }
           }
         }
       }
     }
 
     // Log
-    $this->logSection('info', sprintf('%d URLs where extracted from %d comments', $urls_found_count, count($comments)));
+    $this->logSection('info', sprintf('Inserted %d out of %d extracted URLs from %d comments', $urls_inserted_count, $urls_found_count, count($comments)));
   }
 }
