93a189124479f085b6518784bacbb4512db1715d
Tristan Rivoallan <tristan@rivoallan.net>
2010-06-24 01:17:28 +0200

    [database] Setup initial migration.

--- lib/migration/doctrine/1277334732_addlink.php
+++ lib/migration/doctrine/1277334732_addlink.php
@@ -7,87 +7,125 @@ class Addlink extends Doctrine_Migration_Base
     public function up()
     {
         $this->createTable('link', array(
-             'id' => 
+             'id' =>
              array(
               'type' => 'integer',
               'autoincrement' => true,
               'primary' => true,
               'length' => 8,
              ),
-             'url' => 
+             'url' =>
              array(
               'type' => 'string',
               'notnull' => true,
               'length' => NULL,
              ),
-             'domain_parent' => 
+             'domain_parent' =>
              array(
               'type' => 'string',
               'notnull' => true,
               'length' => NULL,
              ),
-             'domain_fqdn' => 
+             'domain_fqdn' =>
              array(
               'type' => 'string',
               'notnull' => true,
               'length' => NULL,
              ),
-             'mime_type' => 
+             'mime_type' =>
              array(
               'type' => 'string',
               'length' => NULL,
              ),
-             'contributed_at' => 
+             'contributed_at' =>
              array(
               'type' => 'string',
               'length' => NULL,
              ),
-             'contributor_id' => 
+             'contributor_id' =>
              array(
               'type' => 'integer',
               'length' => 8,
              ),
-             'contributor_name' => 
+             'contributor_name' =>
              array(
               'type' => 'string',
               'length' => NULL,
              ),
-             'comment_id' => 
+             'comment_id' =>
              array(
               'type' => 'integer',
               'length' => 8,
              ),
-             'discussion_id' => 
+             'discussion_id' =>
              array(
               'type' => 'integer',
               'length' => 8,
              ),
-             'discussion_name' => 
+             'discussion_name' =>
              array(
               'type' => 'string',
               'length' => NULL,
              ),
-             'created_at' => 
+             'availability' =>
+             array(
+              'type' => 'string',
+              'default' => 'unknown',
+              'length' => NULL,
+             ),
+             'expanded_at' =>
+             array(
+              'type' => 'timestamp',
+              'length' => 25,
+             ),
+             'created_at' =>
              array(
               'notnull' => true,
               'type' => 'timestamp',
               'length' => 25,
              ),
-             'updated_at' => 
+             'updated_at' =>
              array(
               'notnull' => true,
               'type' => 'timestamp',
               'length' => 25,
              ),
              ), array(
-             'indexes' => 
+             'indexes' =>
              array(
+              'idx_url' =>
+              array(
+              'fields' =>
+              array(
+               'url' =>
+               array(
+               'length' => 512,
+               ),
+              ),
+              'type' => 'unique',
+              ),
+              'idx_expanded_at' =>
+              array(
+              'fields' =>
+              array(
+               0 => 'expanded_at',
+              ),
+              ),
+              'idx_availability' =>
+              array(
+              'fields' =>
+              array(
+               'availability' =>
+               array(
+               'length' => 11,
+               ),
+              ),
+              ),
              ),
-             'primary' => 
+             'primary' =>
              array(
               0 => 'id',
              ),
-             'charset' => 'UTF8',
              ));
     }
 
