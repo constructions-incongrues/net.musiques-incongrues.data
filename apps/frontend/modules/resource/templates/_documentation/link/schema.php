ccda7af2f84bac3b5b32f2a9a3f0966b69514fec
Tristan Rivoallan <tristan@rivoallan.net>
2010-06-23 22:34:35 +0200

    [documentation] Documented "availability" links collection attribute.

--- apps/frontend/modules/resource/templates/_documentation/link/schema.php
+++ apps/frontend/modules/resource/templates/_documentation/link/schema.php
@@ -1,9 +1,40 @@
 <p>Cette collection expose les attributs suivants :</p>
 
-<h4 id="schema-url">url</h4>
-<p>C'est l'URL vers la ressource. Par exemple : </p>
-<pre>http://www.glafouk.com/dlz/radioclash_astrotease.mp3</pre>
+<h4 id="schema-availability">availability</h4>
+<p>Ce paramètre correspond à la disponibilité du lien.</p>
+<dl>
+  <dt>Valeurs possibles</dt>
+  <dd><code>unknown</code> : On ne sait pas si l'URL est accessible ou non</dd>
+  <dd><code>available</code> : L'URL est accessible</dd>
+  <dd><code>unavailable</code> : L'URL n'est pas accessible</dd>
+</dl>
+<p>Par défaut, les liens avec une URL non accessible ne sont pas retournés.</p>
 
+<h4 id="schema-comment_id">comment_id</h4>
+<p>C'est l'identifiant du commentaire sur le forum dans lequel à été contribué le lien. Par exemple :</p>
+<pre>15336</pre>
+
+<h4 id="schema-contributed_at">contributed_at</h4>
+<p>C'est la date à laquelle a été contribué le lien. Par exemple :</p>
+<pre>2007-05-09T21:22:05Z</pre>
+
+<h4 id="schema-contributor_name">contributor_name</h4>
+<p>C'est le nom sur le forum de l'utilisateur ayant contribué le lien. Par exemple :</p>
+<pre>mbertier</pre>
+
+<h4 id="schema-contributor_id">contributor_id</h4>
+<p>C'est l'identifiant sur le forum de l'utilisateur ayant contribué le lien. Par exemple :</p>
+<pre>34</pre>
+<p>Les URL pour accéder au profil d'un utilisateur sur Musiques Incongrues ont la forme http://www.musiques-incongrues.net/forum/account/<strong>contributor_id</strong>/</p>
+
+<h4 id="schema-discussion_id">discussion_id</h4>
+<p>C'est l'identifiant de la discussion dans laquelle a été contribué le lien. Par exemple :</p>
+<pre>5455</pre>
+<p>Les URL pour accéder à une discussion sur Musiques Incongrues ont la forme http://www.musiques-incongrues.net/forum/discussion/<strong>discussion_id</strong>/</p>
+
+<h4 id="schema-discussion_name">discussion_name</h4>
+<p>C'est le titre de la discussion dans laquelle a été contribué le lien. Par exemple :</p>
+<pre>Des clips, des clips, rien que des clips</pre>
 <h4 id="schema-domain_fqdn">domain_fqdn</h4>
 <p>C'est le nom de domaine complet de l'URL vers la ressource. Par exemple :</p>
 <pre>data.musiques-incongrues.net</pre>
@@ -17,29 +48,6 @@
 <p>C'est le type MIME de la ressource. Cet attribut n'est pas toujours définit. Il l'est toujours pour les fichiers binaires (mp3, image, etc). Par exemple :</p>
 <pre>audio/mpeg</pre>
 
-<h4 id="schema-contributed_at">contributed_at</h4>
-<p>C'est la date à laquelle a été contribué le lien. Par exemple :</p>
-<pre>2007-05-09T21:22:05Z</pre>
-
-
-<h4 id="schema-contributor_id">contributor_id</h4>
-<p>C'est l'identifiant sur le forum de l'utilisateur ayant contribué le lien. Par exemple :</p>
-<pre>34</pre>
-<p>Les URL pour accéder au profil d'un utilisateur sur Musiques Incongrues ont la forme http://www.musiques-incongrues.net/forum/account/<strong>contributor_id</strong>/</p>
-
-<h4 id="schema-contributor_name">contributor_name</h4>
-<p>C'est le nom sur le forum de l'utilisateur ayant contribué le lien. Par exemple :</p>
-<pre>mbertier</pre>
-
-<h4 id="schema-comment_id">comment_id</h4>
-<p>C'est l'identifiant du commentaire sur le forum dans lequel à été contribué le lien. Par exemple :</p>
-<pre>15336</pre>
-
-<h4 id="schema-discussion_id">discussion_id</h4>
-<p>C'est l'identifiant de la discussion dans laquelle a été contribué le lien. Par exemple :</p>
-<pre>5455</pre>
-<p>Les URL pour accéder à une discussion sur Musiques Incongrues ont la forme http://www.musiques-incongrues.net/forum/discussion/<strong>discussion_id</strong>/</p>
-
-<h4 id="schema-discussion_name">discussion_name</h4>
-<p>C'est le titre de la discussion dans laquelle a été contribué le lien. Par exemple :</p>
-<pre>Des clips, des clips, rien que des clips</pre>
+<h4 id="schema-url">url</h4>
+<p>C'est l'URL vers la ressource. Par exemple : </p>
+<pre>http://www.glafouk.com/dlz/radioclash_astrotease.mp3</pre>
