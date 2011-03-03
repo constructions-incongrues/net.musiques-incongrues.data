0e9c95ca8777e44e44cf4713dc7b8c3d71c3ef49
Tristan Rivoallan <tristan.rivoallan@aramisauto.com>
2011-03-03 22:44:36 +0100

    [results] Reformated html results.

--- apps/frontend/modules/resource/templates/getHtml.php
+++ apps/frontend/modules/resource/templates/getHtml.php
@@ -1,6 +1,12 @@
-<p>Displaying <?php echo count($results) ?> out of <?php echo $results['num_found']?> found results :</p>
-<ul>
-<?php foreach ($results as $resource): ?>
-  <li><pre><?php echo print_r($resource, true) ?></pre></li>
-<?php endforeach; ?>
-</ul>
+<?php use_helper('Text') ?>
+<div class="results html">
+	<p>Displaying <?php echo count($results) - 1  ?> out of <?php echo $results['num_found']?> found results :</p>
+	<?php foreach ($results as $resource): ?>
+	  <dl>
+	  	<dt><?php echo auto_link_text($resource['url']) ?></dt>
+	  	<?php foreach ($resource as $propName => $propValue): ?>
+	  	<dd><strong><?php echo $propName ?> :</strong> <?php echo utf8_decode(auto_link_text($propValue)) ?></dd>
+	  	<?php endforeach; ?>
+	  </dl>
+	<?php endforeach; ?>
+</div>
