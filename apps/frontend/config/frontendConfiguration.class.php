62e227582f4b307fb0660703af62b7cfafbe8a4d
Tristan Rivoallan <tristan@rivoallan.net>
2011-04-15 17:13:19 +0200

    [cache] Link expansion now granulary clears cache.

--- apps/frontend/config/frontendConfiguration.class.php
+++ apps/frontend/config/frontendConfiguration.class.php
@@ -2,7 +2,11 @@
 
 class frontendConfiguration extends sfApplicationConfiguration
 {
-  public function configure()
-  {
-  }
+	public function configure()
+	{
+		// Setup Zend framework autoloading
+		set_include_path(sprintf('%s/vendor/ZendFramework-1.11.4-minimal/library/'.PATH_SEPARATOR.get_include_path(), sfConfig::get('sf_lib_dir')));
+		require_once(sprintf('%s/vendor/ZendFramework-1.11.4-minimal/library/Zend/Loader/Autoloader.php', sfConfig::get('sf_lib_dir')));
+		Zend_Loader_Autoloader::getInstance();
+	}
 }
