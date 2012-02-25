0736d1f7f041b8ad6cf57cd3c373fbae682281ab
Tristan Rivoallan <tristan@rivoallan.net>
2012-02-25 17:16:15 +0100

    [html] added audio player to gallery mode

--- web/js/gallery.behaviors.js
+++ web/js/gallery.behaviors.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 	var regexImages = /\.(jpg|jpeg|gif|png|tif?|pict|bmp)($|\\?)/i;
+	var regexAudio = /\.(mp3|ogg)/i;
 	$('a.oembed').each(function() {
 		if ($(this).attr('href').match(regexImages)) {
 			$(this).replaceWith($('<img src="'+$(this).attr('href')+'" />'));
+		} else if ($(this).attr('href').match(regexAudio)) {
+			$(this).replaceWith($('<a href="'+$(this).attr('href')+'">'+$(this).attr('href').replace(/^.*[\/\\]/g, '')+'</a><br /><audio controls="controls"><source src="'+$(this).attr('href')+'" /></audio>'));
 		} else {
 			$(this).oembed($(this).attr('href'), {
 				'maxWidth': '210px',
